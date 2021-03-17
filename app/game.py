d7ac056c90226c6bf869628a08d299ac7e8211ac
rbeyhum <77794249+rbeyhum@users.noreply.github.com>
2021-03-17 18:04:43 -0400

    Started to reduce complexity

--- app/game.py
+++ app/game.py
@@ -25,23 +25,36 @@ print("COMPUTER CHOICE:", c)
 # DETERMINATION OF WINNER
 #
 
-if u == "rock" and c == "rock":
-    print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
+#if u == "rock" and c == "rock":
+#    print("It's a tie!")
+#elif u == "rock" and c == "paper":
+#    print("The computer wins")
+#elif u == "rock" and c == "scissors":
+#    print("The user wins")
+#
+#elif u == "paper" and c == "rock":
+#    print("The computer wins")
+#elif u == "paper" and c == "paper":
+#    print("It's a tie!")
+#elif u == "paper" and c == "scissors":
+#    print("The user wins")
+#
+#elif u == "scissors" and c == "rock":
+#    print("The computer wins")
+#elif u == "scissors" and c == "paper":
+#    print("The user wins")
+#elif u == "scissors" and c == "scissors":
+#    print("It's a tie!")
 
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
 
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+#if u == "rock":
+#    if c == "rock":
+#        print("____")
+#    elif c == "paper":
+#        print("")
+#    elif c == "scissors":
+#        print(""_____)
+#elif u == "paper":
+#    ...
+
+# there are even less complex ways of doing this (for example using a single dictionary)
