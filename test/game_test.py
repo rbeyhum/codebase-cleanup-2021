8e8946762aaffc26f83bf703af67f7205ee93317
rbeyhum <77794249+rbeyhum@users.noreply.github.com>
2021-04-04 11:00:04 -0400

    Update game_test.py

--- test/game_test.py
+++ test/game_test.py
@@ -9,7 +9,7 @@ from app.game import determine_winner
 def test_determination_of_the_winner():
     assert determine_winner("rock","rock") == None
     assert determine_winner("rock","paper") == "paper"
-    assert determine_winner("rock","scissors") == "scissors"
+    assert determine_winner("rock","scissors") == "rock"
 
     assert determine_winner("paper","rock") == "paper"
     assert determine_winner("paper","paper") == None
